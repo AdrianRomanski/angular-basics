597a168c99004abb2ffcf3ad10ddd2a381071d05
Adrian Romański <adrian.romanski@codeweaver.pl>
2023-04-30 15:41:12 +0200

    template essentials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <div class="app">Hello Angular!</div> `,
+  template: ` <div class="app">
+    <!--sugar syntax-->
+    <h1 #heading (click)="handleClick($event)"> {{message.length ? message : 'Nothing here...'}}</h1>
+    <h1 [innerText]="message.length ? message : 'Nothing here...'"></h1>
+<!--    property binding-->
+    <input #messageInput [value]="message" (input)="handleInput($event)">
+    <p>{{heading.innerText}}</p>
+  </div> `,
   styles: [
     `
       .app {
@@ -15,7 +22,19 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class AppComponent implements OnInit {
+  message: string = 'Hello Angular!'
+  newMessage: string = '';
   ngOnInit() {
+    this.message = 'Hello World';
     console.log('Hello World!');
+  }
+
+  handleClick(event: Event) {
+    console.log('clicked', event);
+  }
+
+  handleInput(event: Event): void {
+    const {value} = event.target as HTMLInputElement;
+    this.newMessage = value;
   }
 }
